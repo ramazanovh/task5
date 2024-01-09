f3be8f5f3c366c4d9049b8392c4393e9dd917ba4
ramazanovh <150849536+ramazanovh@users.noreply.github.com>
2024-01-09 00:26:28 -0800

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿Create.Directory(@".\DataBase");
+
+if (!File.Exists(@".\DataBase\info.txt"))
+{
+    File.Create(@".\DataBase\info.txt");
+}
+bool isContinue = true;
+while (isContinue)
+{
+    Console.WriteLine("\n1)Product yarat");
+    Console.WriteLine("2)Product u goster");
+    Console.WriteLine("--------------------");
+    Console.WriteLine("0)Cixmaq\n");
+    Console.WriteLine("\nChoose an option:\n");
+
+    string? option = Console.ReadLine();
+    int intOption;
+    bool isInt = int.TryParse(option, out intOption);
+    if (isInt)
+    {
+        if (intOption >= 0 && intOption <= 2)
+        {
+            switch (intOption)
+            {
+                case (int)ConsoleApp.Create:
+                    Console.WriteLine("Mehsulun ID ni daxil edin");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Mehsulun Adini daxil edin");
+                    string? name = Console.ReadLine();
+                    Console.WriteLine("Mehsulun qiymetini daxil edin");
+                    decimal price = Convert.ToDecimal(Console.ReadLine());
+                    StreamWriter sw = new StreamWriter(@".\DataBase\product.txt", true);
+                    sw.WriteLine($"Id:{id}, Name: {product.Name}, Price {product.Price}");
+                    sw.Close();
+                    break;
+                case (int)ConsoleApp.GetAll:
+                    StreamReader sr = new StreamReader(@".\DataBas\product.txt");
+                    Console.WriteLine(sr.ReadToEnd());
+                    break;
+                case (int)ConsoleApp.Exit:
+                    isContinue = true;
+                    break;
+            }
+        }
+    }
+}
---
+++ Product/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Product
+{
+    public class Product
+    {
+        private int Id { get; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public Product(int id,string? name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
